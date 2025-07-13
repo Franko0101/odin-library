a8b22092ec0351fa4b3dca92288219233abd7a68
Franko0101 <163135894+Franko0101@users.noreply.github.com>
2025-07-14 00:53:07 +0200

    Working buttons.
    Implemented buttons to remove a book,
    read / unread a book and to open or close
    the sidebar containing a form.

--- script.js
+++ script.js
@@ -26,20 +26,35 @@ addBook(
     false
 )
 
+function removeAllChild(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 function displayBooks() {
-    for(const book of library) {
+    const main = document.querySelector(".main");
+    removeAllChild(main);
+
+    for(let book of library) {
         let bookStatus = "";
         if(book.read) 
             bookStatus = "Letto"
         else 
             bookStatus = "Non Letto";
 
-        const main = document.querySelector(".main");
         const card = document.createElement("div");
         card.classList.add("card");
         const removeBtn = document.createElement("button");
         removeBtn.classList.add("remove-btn");
         removeBtn.innerHTML = "&#10060";
+    
+        removeBtn.addEventListener("click", function (e) {
+            const index = library.findIndex(item => item.id == book.id)
+            library.splice(index, 1);
+            displayBooks();
+        });
+
         const cardText = document.createElement("div");
         cardText.classList.add("card-text");
         const bookTitle = document.createElement("h3");
@@ -51,6 +66,18 @@ function displayBooks() {
         const readStatusBtn = document.createElement("button");
         readStatusBtn.classList.add("read-status-btn");
         readStatusBtn.textContent = bookStatus;
+
+        readStatusBtn.addEventListener("click", () => {
+            if(book.read) {
+                book.read = false;
+                bookStatus = "Non Letto"
+            } else {
+                book.read = true;
+                bookStatus = "Letto"
+            }
+            displayBooks();
+        });
+
         card.appendChild(removeBtn);
         cardText.appendChild(bookTitle);
         cardText.appendChild(bookAuthor);
@@ -65,10 +92,22 @@ const addBookBtn = document.querySelector(".add-book-btn");
 const sidebar = document.querySelector(".sidebar");
 
 addBookBtn.addEventListener("click", () => {
-    if(sidebar.getAttribute("hidden") == null)
+    if(sidebar.getAttribute("hidden") == null) {
         sidebar.setAttribute("hidden", "hidden");
-    else
-        sidebar.removeAttribute("hidden");
+        addBookBtn.textContent = "Add Book"
+    }
+    else {
+        sidebar.removeAttribute("hidden"); 
+        addBookBtn.textContent = "Close"     
+    }
 });
+
+const removeBtns = document.querySelectorAll(".remove-btn");
+
+removeBtns.forEach((button) => {
+    button.addEventListener("click", function (e) {
+        console.log(e.target);
+    })
+})
 
 displayBooks();
